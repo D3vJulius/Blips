ca07f58f53b725c7af483a4ef30cf059486d254a
Julian <julianvictorfjeld@gmail.com>
2021-05-08 16:54:18 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,6 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+author 'D3vJulius / https://github.com/D3vJulius'
+description 'FiveM blips'
+version '1.0.0'
 
 client_script "blips.lua"
